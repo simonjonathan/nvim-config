428512fae514d257969e38acfa5e90edbc0934cf
Jonathan Simon <jonathansi@gmx.de>
2022-12-23 16:53:01 +0100

    test

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("megadutche")
 
-local nvimrc = os.getenv("NVIMRC") -- get nvimrc path vim.cmd('source ' .. nvimrc .. '/lua/config/goyo.vim')
 if vim.g.vscode then
+    local nvimrc = os.getenv("NVIMRC") -- get nvimrc path vim.cmd('source ' .. nvimrc .. '/lua/config/goyo.vim')
     -- VSCode extension
     vim.cmd('source ' .. nvimrc .. '/theme.vim')
 else
