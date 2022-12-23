a500342b59712b1dba6ff769b65fb013818dcd4e
Jonathan Simon <jonathansi@gmx.de>
2022-12-23 17:46:35 +0100

    remove debug

--- init.lua
+++ init.lua
@@ -1,12 +1,6 @@
 require("megadutche")
 
 prefix = os.getenv("HOME") .. '/.config/nvim'
-print(prefix)
-print(os.getenv("HOME"))
-print(os.getenv("NVIMRC"))
-print(prefix)
-print(vim.fn.expand(prefix))
-print('source ' .. vim.fn.expand(prefix) .. '/theme.vim')
 if vim.g.vscode then
     -- VSCode extension
     vim.cmd('source ' .. vim.fn.expand(prefix) .. '/theme.vim')
