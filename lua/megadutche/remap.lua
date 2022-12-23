157ad4a9c1b51b21420a2ff61ad88e27c180e632
Jonathan Simon <jonathansi@gmx.de>
2022-12-23 22:59:23 +0100

    add lsp, add some keymaps and set config

--- lua/megadutche/remap.lua
+++ lua/megadutche/remap.lua
@@ -10,4 +10,23 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- make C-c behave like Esc
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+
+-- next greatest remap ever : asbjornHaland
+-- copy into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- delete into void
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+
 vim.keymap.set("n", "Q", "<nop>")
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- lua/megadutche/set.lua
+++ lua/megadutche/set.lua
@@ -1,6 +1,7 @@
-vim.opt.clipboard = vim.opt.clipboard + 'unnamedplus'
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 
-vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -8,10 +9,22 @@ vim.opt.incsearch = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
 vim.opt.smartindent = true
 
-vim.opt.scrolloff = 16
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
