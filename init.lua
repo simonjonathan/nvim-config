a5c0013f1ce72e276011e117c988cf70d8c55096
Jonathan Simon <jonathansi@gmx.de>
2022-12-23 17:15:49 +0100

    debug

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
 require("megadutche")
 
-if vim.fn.has('win32')
-   local prefix = $HOME . '/AppData/Local/nvim'
+if vim.fn.has('win32') then
+   local prefix = vim.env.HOME .. '/AppData/Local/nvim'
 else
-   local prefix = $HOME . '/.config/nvim'
+   local prefix = vim.env.HOME .. '/.config/nvim'
 end
 
+print(prefix)
+print(vim.fn.expand(prefix))
+print('source ' .. vim.fn.expand(prefix) .. '/theme.vim')
 if vim.g.vscode then
     -- VSCode extension
     vim.cmd('source ' .. vim.fn.expand(prefix) .. '/theme.vim')
